23dacae9441e18ef4e1782e0af0273f0186d38ff
biwa <6475593+biwa@users.noreply.github.com>
2020-12-30 22:13:34 +0100

    OBJ exporter: changed tab order to make sense

--- Source/Plugins/BuilderModes/Interface/WavefrontSettingsForm.Designer.cs
+++ Source/Plugins/BuilderModes/Interface/WavefrontSettingsForm.Designer.cs
@@ -39,6 +39,7 @@
 			this.nudScale = new System.Windows.Forms.NumericUpDown();
 			this.label2 = new System.Windows.Forms.Label();
 			this.gbGZDoom = new System.Windows.Forms.GroupBox();
+			this.actorNameError = new System.Windows.Forms.PictureBox();
 			this.groupBox3 = new System.Windows.Forms.GroupBox();
 			this.rbZScript = new System.Windows.Forms.RadioButton();
 			this.rbDecorate = new System.Windows.Forms.RadioButton();
@@ -52,9 +53,12 @@
 			this.tbActorName = new System.Windows.Forms.TextBox();
 			this.label6 = new System.Windows.Forms.Label();
 			this.label5 = new System.Windows.Forms.Label();
+			this.bBrowseModelPath = new System.Windows.Forms.Button();
 			this.tbModelPath = new System.Windows.Forms.TextBox();
+			this.bBrowseActorPath = new System.Windows.Forms.Button();
 			this.tbActorPath = new System.Windows.Forms.TextBox();
 			this.label4 = new System.Windows.Forms.Label();
+			this.bBrowseBasePath = new System.Windows.Forms.Button();
 			this.label3 = new System.Windows.Forms.Label();
 			this.tbBasePath = new System.Windows.Forms.TextBox();
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
@@ -67,19 +71,15 @@
 			this.cbNormalizeLowestVertex = new System.Windows.Forms.CheckBox();
 			this.cbCenterModel = new System.Windows.Forms.CheckBox();
 			this.cbIgnoreControlSectors = new System.Windows.Forms.CheckBox();
-			this.bBrowseModelPath = new System.Windows.Forms.Button();
-			this.bBrowseActorPath = new System.Windows.Forms.Button();
-			this.bBrowseBasePath = new System.Windows.Forms.Button();
 			this.browse = new System.Windows.Forms.Button();
-			this.actorNameError = new System.Windows.Forms.PictureBox();
 			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
 			((System.ComponentModel.ISupportInitialize)(this.nudScale)).BeginInit();
 			this.gbGZDoom.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.actorNameError)).BeginInit();
 			this.groupBox3.SuspendLayout();
 			this.groupBox2.SuspendLayout();
 			this.groupBox1.SuspendLayout();
 			this.groupBox4.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.actorNameError)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// tbExportPath
@@ -87,7 +87,7 @@
 			this.tbExportPath.Location = new System.Drawing.Point(55, 12);
 			this.tbExportPath.Name = "tbExportPath";
 			this.tbExportPath.Size = new System.Drawing.Size(344, 20);
-			this.tbExportPath.TabIndex = 0;
+			this.tbExportPath.TabIndex = 1;
 			// 
 			// label1
 			// 
@@ -95,7 +95,7 @@
 			this.label1.Location = new System.Drawing.Point(18, 14);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(32, 13);
-			this.label1.TabIndex = 2;
+			this.label1.TabIndex = 0;
 			this.label1.Text = "Path:";
 			// 
 			// cbExportForGZDoom
@@ -104,7 +104,7 @@
 			this.cbExportForGZDoom.Location = new System.Drawing.Point(6, 0);
 			this.cbExportForGZDoom.Name = "cbExportForGZDoom";
 			this.cbExportForGZDoom.Size = new System.Drawing.Size(117, 17);
-			this.cbExportForGZDoom.TabIndex = 3;
+			this.cbExportForGZDoom.TabIndex = 0;
 			this.cbExportForGZDoom.Text = "Export for GZDoom";
 			this.cbExportForGZDoom.UseVisualStyleBackColor = true;
 			this.cbExportForGZDoom.CheckedChanged += new System.EventHandler(this.cbFixScale_CheckedChanged);
@@ -115,7 +115,7 @@
 			this.export.Location = new System.Drawing.Point(279, 518);
 			this.export.Name = "export";
 			this.export.Size = new System.Drawing.Size(75, 23);
-			this.export.TabIndex = 4;
+			this.export.TabIndex = 7;
 			this.export.Text = "Export";
 			this.export.UseVisualStyleBackColor = true;
 			this.export.Click += new System.EventHandler(this.export_Click);
@@ -127,7 +127,7 @@
 			this.cancel.Location = new System.Drawing.Point(360, 518);
 			this.cancel.Name = "cancel";
 			this.cancel.Size = new System.Drawing.Size(75, 23);
-			this.cancel.TabIndex = 5;
+			this.cancel.TabIndex = 8;
 			this.cancel.Text = "Cancel";
 			this.cancel.UseVisualStyleBackColor = true;
 			this.cancel.Click += new System.EventHandler(this.cancel_Click);
@@ -144,7 +144,7 @@
 			this.cbExportTextures.Location = new System.Drawing.Point(200, 19);
 			this.cbExportTextures.Name = "cbExportTextures";
 			this.cbExportTextures.Size = new System.Drawing.Size(96, 17);
-			this.cbExportTextures.TabIndex = 6;
+			this.cbExportTextures.TabIndex = 2;
 			this.cbExportTextures.Text = "Export textures";
 			this.cbExportTextures.UseVisualStyleBackColor = true;
 			// 
@@ -164,7 +164,7 @@
             -2147483648});
 			this.nudScale.Name = "nudScale";
 			this.nudScale.Size = new System.Drawing.Size(94, 20);
-			this.nudScale.TabIndex = 8;
+			this.nudScale.TabIndex = 4;
 			this.nudScale.Value = new decimal(new int[] {
             1,
             0,
@@ -177,7 +177,7 @@
 			this.label2.Location = new System.Drawing.Point(12, 40);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(37, 13);
-			this.label2.TabIndex = 9;
+			this.label2.TabIndex = 3;
 			this.label2.Text = "Scale:";
 			// 
 			// gbGZDoom
@@ -202,8 +202,18 @@
 			this.gbGZDoom.Location = new System.Drawing.Point(15, 141);
 			this.gbGZDoom.Name = "gbGZDoom";
 			this.gbGZDoom.Size = new System.Drawing.Size(420, 368);
-			this.gbGZDoom.TabIndex = 10;
+			this.gbGZDoom.TabIndex = 6;
 			this.gbGZDoom.TabStop = false;
+			// 
+			// actorNameError
+			// 
+			this.actorNameError.Image = global::CodeImp.DoomBuilder.BuilderModes.Properties.Resources.Warning;
+			this.actorNameError.Location = new System.Drawing.Point(376, 25);
+			this.actorNameError.Name = "actorNameError";
+			this.actorNameError.Size = new System.Drawing.Size(16, 16);
+			this.actorNameError.TabIndex = 24;
+			this.actorNameError.TabStop = false;
+			this.actorNameError.Visible = false;
 			// 
 			// groupBox3
 			// 
@@ -212,7 +222,7 @@
 			this.groupBox3.Location = new System.Drawing.Point(8, 286);
 			this.groupBox3.Name = "groupBox3";
 			this.groupBox3.Size = new System.Drawing.Size(170, 73);
-			this.groupBox3.TabIndex = 23;
+			this.groupBox3.TabIndex = 14;
 			this.groupBox3.TabStop = false;
 			this.groupBox3.Text = "Actor format";
 			// 
@@ -223,7 +233,7 @@
 			this.rbZScript.Location = new System.Drawing.Point(6, 20);
 			this.rbZScript.Name = "rbZScript";
 			this.rbZScript.Size = new System.Drawing.Size(59, 17);
-			this.rbZScript.TabIndex = 21;
+			this.rbZScript.TabIndex = 0;
 			this.rbZScript.TabStop = true;
 			this.rbZScript.Text = "ZScript";
 			this.rbZScript.UseVisualStyleBackColor = true;
@@ -234,7 +244,7 @@
 			this.rbDecorate.Location = new System.Drawing.Point(6, 44);
 			this.rbDecorate.Name = "rbDecorate";
 			this.rbDecorate.Size = new System.Drawing.Size(84, 17);
-			this.rbDecorate.TabIndex = 22;
+			this.rbDecorate.TabIndex = 1;
 			this.rbDecorate.TabStop = true;
 			this.rbDecorate.Text = "DECORATE";
 			this.rbDecorate.UseVisualStyleBackColor = true;
@@ -249,7 +259,7 @@
 			this.groupBox2.Location = new System.Drawing.Point(9, 158);
 			this.groupBox2.Name = "groupBox2";
 			this.groupBox2.Size = new System.Drawing.Size(169, 119);
-			this.groupBox2.TabIndex = 18;
+			this.groupBox2.TabIndex = 13;
 			this.groupBox2.TabStop = false;
 			this.groupBox2.Text = "Actor settings";
 			// 
@@ -307,7 +317,7 @@
 			this.bResetPaths.Location = new System.Drawing.Point(74, 127);
 			this.bResetPaths.Name = "bResetPaths";
 			this.bResetPaths.Size = new System.Drawing.Size(75, 23);
-			this.bResetPaths.TabIndex = 16;
+			this.bResetPaths.TabIndex = 12;
 			this.bResetPaths.Text = "Reset paths";
 			this.bResetPaths.UseVisualStyleBackColor = true;
 			this.bResetPaths.Click += new System.EventHandler(this.bResetPaths_Click);
@@ -317,7 +327,7 @@
 			this.tbActorName.Location = new System.Drawing.Point(74, 23);
 			this.tbActorName.Name = "tbActorName";
 			this.tbActorName.Size = new System.Drawing.Size(296, 20);
-			this.tbActorName.TabIndex = 15;
+			this.tbActorName.TabIndex = 2;
 			this.tbActorName.TextChanged += new System.EventHandler(this.tbActorName_TextChanged);
 			// 
 			// label6
@@ -326,7 +336,7 @@
 			this.label6.Location = new System.Drawing.Point(4, 26);
 			this.label6.Name = "label6";
 			this.label6.Size = new System.Drawing.Size(64, 13);
-			this.label6.TabIndex = 14;
+			this.label6.TabIndex = 1;
 			this.label6.Text = "Actor name:";
 			// 
 			// label5
@@ -335,22 +345,42 @@
 			this.label5.Location = new System.Drawing.Point(5, 104);
 			this.label5.Name = "label5";
 			this.label5.Size = new System.Drawing.Size(63, 13);
-			this.label5.TabIndex = 13;
+			this.label5.TabIndex = 9;
 			this.label5.Text = "Model path:";
+			// 
+			// bBrowseModelPath
+			// 
+			this.bBrowseModelPath.Image = global::CodeImp.DoomBuilder.BuilderModes.Properties.Resources.Folder;
+			this.bBrowseModelPath.Location = new System.Drawing.Point(376, 99);
+			this.bBrowseModelPath.Name = "bBrowseModelPath";
+			this.bBrowseModelPath.Size = new System.Drawing.Size(30, 24);
+			this.bBrowseModelPath.TabIndex = 11;
+			this.bBrowseModelPath.UseVisualStyleBackColor = true;
+			this.bBrowseModelPath.Click += new System.EventHandler(this.bBrowseModelPath_Click);
 			// 
 			// tbModelPath
 			// 
 			this.tbModelPath.Location = new System.Drawing.Point(74, 101);
 			this.tbModelPath.Name = "tbModelPath";
 			this.tbModelPath.Size = new System.Drawing.Size(296, 20);
-			this.tbModelPath.TabIndex = 11;
+			this.tbModelPath.TabIndex = 10;
+			// 
+			// bBrowseActorPath
+			// 
+			this.bBrowseActorPath.Image = global::CodeImp.DoomBuilder.BuilderModes.Properties.Resources.Folder;
+			this.bBrowseActorPath.Location = new System.Drawing.Point(376, 73);
+			this.bBrowseActorPath.Name = "bBrowseActorPath";
+			this.bBrowseActorPath.Size = new System.Drawing.Size(30, 24);
+			this.bBrowseActorPath.TabIndex = 8;
+			this.bBrowseActorPath.UseVisualStyleBackColor = true;
+			this.bBrowseActorPath.Click += new System.EventHandler(this.bBrowseActorPath_Click);
 			// 
 			// tbActorPath
 			// 
 			this.tbActorPath.Location = new System.Drawing.Point(74, 75);
 			this.tbActorPath.Name = "tbActorPath";
 			this.tbActorPath.Size = new System.Drawing.Size(296, 20);
-			this.tbActorPath.TabIndex = 9;
+			this.tbActorPath.TabIndex = 7;
 			// 
 			// label4
 			// 
@@ -358,8 +388,18 @@
 			this.label4.Location = new System.Drawing.Point(9, 78);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(59, 13);
-			this.label4.TabIndex = 8;
+			this.label4.TabIndex = 6;
 			this.label4.Text = "Actor path:";
+			// 
+			// bBrowseBasePath
+			// 
+			this.bBrowseBasePath.Image = global::CodeImp.DoomBuilder.BuilderModes.Properties.Resources.Folder;
+			this.bBrowseBasePath.Location = new System.Drawing.Point(376, 47);
+			this.bBrowseBasePath.Name = "bBrowseBasePath";
+			this.bBrowseBasePath.Size = new System.Drawing.Size(30, 24);
+			this.bBrowseBasePath.TabIndex = 5;
+			this.bBrowseBasePath.UseVisualStyleBackColor = true;
+			this.bBrowseBasePath.Click += new System.EventHandler(this.bBrowseBasePath_Click);
 			// 
 			// label3
 			// 
@@ -367,7 +407,7 @@
 			this.label3.Location = new System.Drawing.Point(10, 52);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(58, 13);
-			this.label3.TabIndex = 6;
+			this.label3.TabIndex = 3;
 			this.label3.Text = "Base path:";
 			// 
 			// tbBasePath
@@ -375,7 +415,7 @@
 			this.tbBasePath.Location = new System.Drawing.Point(74, 49);
 			this.tbBasePath.Name = "tbBasePath";
 			this.tbBasePath.Size = new System.Drawing.Size(296, 20);
-			this.tbBasePath.TabIndex = 5;
+			this.tbBasePath.TabIndex = 4;
 			// 
 			// groupBox1
 			// 
@@ -386,7 +426,7 @@
 			this.groupBox1.Location = new System.Drawing.Point(184, 158);
 			this.groupBox1.Name = "groupBox1";
 			this.groupBox1.Size = new System.Drawing.Size(222, 201);
-			this.groupBox1.TabIndex = 4;
+			this.groupBox1.TabIndex = 15;
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "Skip textures";
 			// 
@@ -395,7 +435,7 @@
 			this.bRemoveTexture.Location = new System.Drawing.Point(7, 140);
 			this.bRemoveTexture.Name = "bRemoveTexture";
 			this.bRemoveTexture.Size = new System.Drawing.Size(209, 23);
-			this.bRemoveTexture.TabIndex = 3;
+			this.bRemoveTexture.TabIndex = 1;
 			this.bRemoveTexture.Text = "Remove selected";
 			this.bRemoveTexture.UseVisualStyleBackColor = true;
 			this.bRemoveTexture.Click += new System.EventHandler(this.bRemoveTexture_Click);
@@ -405,7 +445,7 @@
 			this.bAddFlat.Location = new System.Drawing.Point(114, 169);
 			this.bAddFlat.Name = "bAddFlat";
 			this.bAddFlat.Size = new System.Drawing.Size(102, 23);
-			this.bAddFlat.TabIndex = 2;
+			this.bAddFlat.TabIndex = 3;
 			this.bAddFlat.Text = "Add flat";
 			this.bAddFlat.UseVisualStyleBackColor = true;
 			this.bAddFlat.Click += new System.EventHandler(this.bAddFlat_Click);
@@ -415,7 +455,7 @@
 			this.bAddTexture.Location = new System.Drawing.Point(7, 169);
 			this.bAddTexture.Name = "bAddTexture";
 			this.bAddTexture.Size = new System.Drawing.Size(102, 23);
-			this.bAddTexture.TabIndex = 1;
+			this.bAddTexture.TabIndex = 2;
 			this.bAddTexture.Text = "Add texture";
 			this.bAddTexture.UseVisualStyleBackColor = true;
 			this.bAddTexture.Click += new System.EventHandler(this.bAddTexture_Click);
@@ -438,7 +478,7 @@
 			this.groupBox4.Location = new System.Drawing.Point(15, 64);
 			this.groupBox4.Name = "groupBox4";
 			this.groupBox4.Size = new System.Drawing.Size(420, 71);
-			this.groupBox4.TabIndex = 12;
+			this.groupBox4.TabIndex = 5;
 			this.groupBox4.TabStop = false;
 			this.groupBox4.Text = "General settings";
 			// 
@@ -450,7 +490,7 @@
 			this.cbNormalizeLowestVertex.Location = new System.Drawing.Point(6, 44);
 			this.cbNormalizeLowestVertex.Name = "cbNormalizeLowestVertex";
 			this.cbNormalizeLowestVertex.Size = new System.Drawing.Size(166, 17);
-			this.cbNormalizeLowestVertex.TabIndex = 19;
+			this.cbNormalizeLowestVertex.TabIndex = 1;
 			this.cbNormalizeLowestVertex.Text = "Normalize lowest vertex z to 0";
 			this.cbNormalizeLowestVertex.UseVisualStyleBackColor = true;
 			// 
@@ -462,7 +502,7 @@
 			this.cbCenterModel.Location = new System.Drawing.Point(6, 21);
 			this.cbCenterModel.Name = "cbCenterModel";
 			this.cbCenterModel.Size = new System.Drawing.Size(88, 17);
-			this.cbCenterModel.TabIndex = 20;
+			this.cbCenterModel.TabIndex = 0;
 			this.cbCenterModel.Text = "Center model";
 			this.cbCenterModel.UseVisualStyleBackColor = true;
 			// 
@@ -474,39 +514,9 @@
 			this.cbIgnoreControlSectors.Location = new System.Drawing.Point(200, 42);
 			this.cbIgnoreControlSectors.Name = "cbIgnoreControlSectors";
 			this.cbIgnoreControlSectors.Size = new System.Drawing.Size(168, 17);
-			this.cbIgnoreControlSectors.TabIndex = 17;
+			this.cbIgnoreControlSectors.TabIndex = 3;
 			this.cbIgnoreControlSectors.Text = "Ignore 3D floor control sectors";
 			this.cbIgnoreControlSectors.UseVisualStyleBackColor = true;
-			// 
-			// bBrowseModelPath
-			// 
-			this.bBrowseModelPath.Image = global::CodeImp.DoomBuilder.BuilderModes.Properties.Resources.Folder;
-			this.bBrowseModelPath.Location = new System.Drawing.Point(376, 99);
-			this.bBrowseModelPath.Name = "bBrowseModelPath";
-			this.bBrowseModelPath.Size = new System.Drawing.Size(30, 24);
-			this.bBrowseModelPath.TabIndex = 12;
-			this.bBrowseModelPath.UseVisualStyleBackColor = true;
-			this.bBrowseModelPath.Click += new System.EventHandler(this.bBrowseModelPath_Click);
-			// 
-			// bBrowseActorPath
-			// 
-			this.bBrowseActorPath.Image = global::CodeImp.DoomBuilder.BuilderModes.Properties.Resources.Folder;
-			this.bBrowseActorPath.Location = new System.Drawing.Point(376, 73);
-			this.bBrowseActorPath.Name = "bBrowseActorPath";
-			this.bBrowseActorPath.Size = new System.Drawing.Size(30, 24);
-			this.bBrowseActorPath.TabIndex = 10;
-			this.bBrowseActorPath.UseVisualStyleBackColor = true;
-			this.bBrowseActorPath.Click += new System.EventHandler(this.bBrowseActorPath_Click);
-			// 
-			// bBrowseBasePath
-			// 
-			this.bBrowseBasePath.Image = global::CodeImp.DoomBuilder.BuilderModes.Properties.Resources.Folder;
-			this.bBrowseBasePath.Location = new System.Drawing.Point(376, 47);
-			this.bBrowseBasePath.Name = "bBrowseBasePath";
-			this.bBrowseBasePath.Size = new System.Drawing.Size(30, 24);
-			this.bBrowseBasePath.TabIndex = 7;
-			this.bBrowseBasePath.UseVisualStyleBackColor = true;
-			this.bBrowseBasePath.Click += new System.EventHandler(this.bBrowseBasePath_Click);
 			// 
 			// browse
 			// 
@@ -514,19 +524,9 @@
 			this.browse.Location = new System.Drawing.Point(405, 10);
 			this.browse.Name = "browse";
 			this.browse.Size = new System.Drawing.Size(30, 24);
-			this.browse.TabIndex = 1;
+			this.browse.TabIndex = 2;
 			this.browse.UseVisualStyleBackColor = true;
 			this.browse.Click += new System.EventHandler(this.browse_Click);
-			// 
-			// actorNameError
-			// 
-			this.actorNameError.Image = global::CodeImp.DoomBuilder.BuilderModes.Properties.Resources.Warning;
-			this.actorNameError.Location = new System.Drawing.Point(376, 25);
-			this.actorNameError.Name = "actorNameError";
-			this.actorNameError.Size = new System.Drawing.Size(16, 16);
-			this.actorNameError.TabIndex = 24;
-			this.actorNameError.TabStop = false;
-			this.actorNameError.Visible = false;
 			// 
 			// WavefrontSettingsForm
 			// 
@@ -555,6 +555,7 @@
 			((System.ComponentModel.ISupportInitialize)(this.nudScale)).EndInit();
 			this.gbGZDoom.ResumeLayout(false);
 			this.gbGZDoom.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.actorNameError)).EndInit();
 			this.groupBox3.ResumeLayout(false);
 			this.groupBox3.PerformLayout();
 			this.groupBox2.ResumeLayout(false);
@@ -562,7 +563,6 @@
 			this.groupBox1.ResumeLayout(false);
 			this.groupBox4.ResumeLayout(false);
 			this.groupBox4.PerformLayout();
-			((System.ComponentModel.ISupportInitialize)(this.actorNameError)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
