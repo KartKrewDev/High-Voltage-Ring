b77bb9fe6fb98e62acf80deeb4094e9f744819e3
biwa <6475593+biwa@users.noreply.github.com>
2021-08-24 20:25:36 +0200

    Visual Mode: fixed a problem where highlighted visual slope handles were not highlighted anymore after clearing the selection

--- Source/Plugins/BuilderModes/VisualModes/BaseVisualMode.cs
+++ Source/Plugins/BuilderModes/VisualModes/BaseVisualMode.cs
@@ -523,6 +523,7 @@ namespace CodeImp.DoomBuilder.BuilderModes
 			// Should we update the info on panels?
 			bool updateinfo = (newtarget.picked != target.picked);
 
+			// Operating on slope handles is potentially expensive, so only do it it absolutely necessary (i.e. when a new slope handle was selected)
 			if (updateinfo)
 			{
 				if (target.picked is VisualSlope) // Old target
@@ -2488,10 +2489,16 @@ namespace CodeImp.DoomBuilder.BuilderModes
 			{
 				if (General.Map.UDMF)
 				{
+					VisualSlope oldsmartpivot = null;
+
 					foreach (KeyValuePair<Sector, List<VisualSlope>> kvp in allslopehandles)
 					{
 						foreach (VisualSlope handle in kvp.Value)
 						{
+							// We want to keep the old smart pivot handle
+							if (handle.SmartPivot)
+								oldsmartpivot = handle;
+
 							handle.Selected = false;
 							handle.Pivot = false;
 							handle.SmartPivot = false;
@@ -2499,6 +2506,20 @@ namespace CodeImp.DoomBuilder.BuilderModes
 					}
 
 					usedslopehandles.Clear();
+
+					// Clearing the used slopes also clears the currently highlighted handle and the smart pivot handle. For
+					// performance reasons PickTarget() will not do its slope handle stuff if the current and new pick are
+					// the same, so we need to re-add the currently picked slope handle and its smart pivot handle
+					if (target.picked is VisualSlope)
+					{
+						usedslopehandles.Add((VisualSlope)target.picked);
+
+						if (oldsmartpivot != null)
+						{
+							oldsmartpivot.SmartPivot = true;
+							usedslopehandles.Add(oldsmartpivot);
+						}
+					}
 				}
 			}
 
