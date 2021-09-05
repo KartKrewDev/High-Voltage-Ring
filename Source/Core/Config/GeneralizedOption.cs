KartKrewDev/High-Voltage-Ring

commit ea37ed18fcf179a3ab1f04d4428c0f8fe2402ab2

Fixed a problem where generalized effects could output a warning if they are made up of multiple bits (like in the MBF21 generalized sector effects)

Source/Core/Config/GeneralizedOption.cs
@@ -90,6 +90,9 @@ namespace CodeImp.DoomBuilder.Config
 			bits.Sort();
 
 			//mxd. Determine and check increment steps
+			// biwa. Setting this to be in debug build only. There are valid scenarios where this isn't a problem, specifically MBF21,
+			// where the "alternate damage mode" for sectors is made up of 3 bits.
+			#if DEBUG
 			if(bits.Count > 1)
 			{
 				// Use the second bit as the structure's step
@@ -102,7 +105,8 @@ namespace CodeImp.DoomBuilder.Config
 						General.ErrorLogger.Add(ErrorType.Warning, "Structure \"" + fullpath + "." + name + "\" contains options with mixed increments (option \"" + bits[i].Title + "\" increment (" + (bits[i - 1].Index - bits[i].Index) + ") doesn't match the structure increment (" + bitstep + ")).");
 				}
 			}
-			
+			#endif
+
 			// We have no destructor
 			GC.SuppressFinalize(this);
 		}

commit d766f27e75c137e4d704095424aff5b122996f7d

Fixed a bug with generalized options introduced in ea37ed1

Source/Core/Config/GeneralizedOption.cs
@@ -92,28 +92,29 @@ namespace CodeImp.DoomBuilder.Config
 			//mxd. Determine and check increment steps
 			// biwa. Setting this to be in debug build only. There are valid scenarios where this isn't a problem, specifically MBF21,
 			// where the "alternate damage mode" for sectors is made up of 3 bits.
-			#if DEBUG
+			
 			if(bits.Count > 1)
 			{
 				// Use the second bit as the structure's step
-				bitstep = bits[1].Index; 
-				
+				bitstep = bits[1].Index;
+
+				#if DEBUG
 				// Check the rest of the values
 				for(int i = 1; i < bits.Count; i++)
 				{
 					if(bits[i].Index - bits[i - 1].Index != bitstep)
 						General.ErrorLogger.Add(ErrorType.Warning, "Structure \"" + fullpath + "." + name + "\" contains options with mixed increments (option \"" + bits[i].Title + "\" increment (" + (bits[i - 1].Index - bits[i].Index) + ") doesn't match the structure increment (" + bitstep + ")).");
 				}
+				#endif
 			}
-			#endif
 
 			// We have no destructor
 			GC.SuppressFinalize(this);
 		}
 
-		#endregion
+#endregion
 
-		#region ================== Methods
+#region ================== Methods
 
 		// This presents the item as string
 		public override string ToString()
@@ -121,6 +122,6 @@ namespace CodeImp.DoomBuilder.Config
 			return name;
 		}
 		
-		#endregion
+#endregion
 	}
 }